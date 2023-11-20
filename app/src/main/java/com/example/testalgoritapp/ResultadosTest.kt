17705633f95b6329e2c6aa18a75536600672e303
LuuDev <luudev@MacBook-Pro-de-LuuDev.local>
2023-11-20 15:44:54 -0800

    primer commit

---
+++ app/src/main/java/com/example/testalgoritapp/ResultadosTest.kt
@@ -0,0 +1,11 @@
+package com.example.testalgoritapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadosTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultados_test)
+    }
+}
---
+++ app/src/main/java/com/example/testalgoritapp/TestData.kt
@@ -0,0 +1,11 @@
+package com.example.testalgoritapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_data);
+    }
+}
